5417b1441dbded0180a459c99b8e75b52b8c6a0b
Yu Cheong <CheongYX@cardiff.ac.uk>
2023-09-11 16:32:12 +0800

    refactor

--- main.go
+++ main.go
@@ -1,19 +1,12 @@
 package main
 
 import (
-	"embed"
-	"fmt"
 	"log"
 	"net/http"
-	"strings"
 
 	"github.com/fidraC/canary/api/fingerprinting"
+	"github.com/fidraC/canary/static"
 )
-
-// Embed static/ directory into binary
-//
-//go:embed static
-var staticFS embed.FS
 
 func main() {
 	fingerprinter := fingerprinting.NewHandler()
@@ -23,37 +16,7 @@ func main() {
 	}
 	router := http.NewServeMux()
 	router.HandleFunc("/finger", fingerprinter.ServeHTTP)
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		var file []byte
-		var err error
-		if r.URL.Path == "/" {
-			r.URL.Path = "/index.html"
-		}
-		file, err = staticFS.ReadFile(fmt.Sprintf("static%s", r.URL.Path))
-		if err != nil {
-			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte(err.Error()))
-			return
-		}
-		// Determine content type based on file extension
-		contentType := "text/plain"
-		switch {
-		case strings.HasSuffix(r.URL.Path, ".html"):
-			contentType = "text/html"
-		case strings.HasSuffix(r.URL.Path, ".css"):
-			contentType = "text/css"
-		case strings.HasSuffix(r.URL.Path, ".js"):
-			contentType = "application/javascript"
-		case strings.HasSuffix(r.URL.Path, ".png"):
-			contentType = "image/png"
-		case strings.HasSuffix(r.URL.Path, ".ico"):
-			contentType = "image/x-icon"
-		}
-		w.Header().Set("Content-Type", contentType)
-
-		w.Write(file)
-
-	})
+	router.HandleFunc("/", static.StaticHandler)
 	log.Println("Listening on :4443")
 	http.Serve(listener, router)
 
---
+++ static/static.go
@@ -0,0 +1,43 @@
+package static
+
+import (
+	"embed"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+//go:embed dist
+var staticFS embed.FS
+
+func StaticHandler(w http.ResponseWriter, r *http.Request) {
+	var file []byte
+	var err error
+	if r.URL.Path == "/" {
+		r.URL.Path = "/index.html"
+	}
+	file, err = staticFS.ReadFile(fmt.Sprintf("static%s", r.URL.Path))
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	// Determine content type based on file extension
+	contentType := "text/plain"
+	switch {
+	case strings.HasSuffix(r.URL.Path, ".html"):
+		contentType = "text/html"
+	case strings.HasSuffix(r.URL.Path, ".css"):
+		contentType = "text/css"
+	case strings.HasSuffix(r.URL.Path, ".js"):
+		contentType = "application/javascript"
+	case strings.HasSuffix(r.URL.Path, ".png"):
+		contentType = "image/png"
+	case strings.HasSuffix(r.URL.Path, ".ico"):
+		contentType = "image/x-icon"
+	}
+	w.Header().Set("Content-Type", contentType)
+
+	w.Write(file)
+
+}
